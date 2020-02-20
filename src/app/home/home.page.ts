103458df3c8e39021c5a10d2a3570df411d0ec2b
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2020-02-20 13:39:12 +0100

    add product list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PRODUCTS } from '../mock-products';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  products = PRODUCTS;
 
-  constructor() {}
+  constructor() { }
 
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      event.target.complete();
+
+      this.products = [...this.products, ...PRODUCTS];
+
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+      if (this.products.length >= 50) {
+        event.target.disabled = true;
+      }
+    }, 500);
+  }
 }
